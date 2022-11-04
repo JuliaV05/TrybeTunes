00055ff6dbe266ab87afd1e3f47a76b4541dd141
Júlia Viana <juliavianamendes05@gmail.com>
2022-11-04 19:23:07 -0300

    desenvolvendo requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          <input data-testid="login-name-input" />
+          <div>
+            <label htmlFor="button-login">
+              Entrar
+              <input type="button" data-testid="login-submit-button" />
+            </label>
+          </div>
+        </form>
+      </div>
     );
   }
 }
