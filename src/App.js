c5c34d4c23e3e7bdcf0aa5bc67a9f9d0d0a279e7
Júlia Viana <juliavianamendes05@gmail.com>
2022-11-07 22:01:47 -0300

    requisito 2 desenvolvido

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
-import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
@@ -45,7 +45,7 @@ class App extends React.Component {
           />
           <Route
             exact
-            path=""
+            path="/*"
             component={ NotFound }
           />
         </Switch>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,74 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      buttonDisabled: true,
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.userChar);
+  };
+
+  textLoading = async () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name });
+    this.setState({ loading: false, redirect: true });
+  };
+
+  userChar = () => {
+    const number = 3;
+    const { name } = this.state;
+    if (name.length >= number) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
   render() {
+    const { name, buttonDisabled, loading, redirect } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    if (redirect) {
+      return <Redirect to="/search" />;
+    }
     return (
       <div data-testid="page-login">
         <form>
-          <input data-testid="login-name-input" />
+          <label htmlFor="login-name-input">
+            <input
+              type="text"
+              name="name"
+              value={ name }
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+          </label>
           <div>
-            <label htmlFor="button-login">
+
+            <button
+              type="button"
+              disabled={ buttonDisabled }
+              onClick={ this.textLoading }
+              data-testid="login-submit-button"
+            >
               Entrar
-              <input type="button" data-testid="login-submit-button" />
-            </label>
+            </button>
+
           </div>
         </form>
       </div>
