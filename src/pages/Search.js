cd68753019b8bb2f9ff562f6dcdd4a768578190d
Júlia Viana <juliavianamendes05@gmail.com>
2022-11-09 17:43:31 -0300

    requisito 5 desenvolvido

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  state = {
+    buttonDisabled: true,
+    name: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.searchArtist);
+  };
+
+  searchArtist = () => {
+    const number = 2;
+    const { name } = this.state;
+    if (name.length >= number) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
   render() {
+    const { name, buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        <form>
+          <input
+            name="name"
+            type="text"
+            value={ name }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
